b43eb7ef1f7a83ae9f3fb27c1ac6c7a66b639168
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2019-12-20 16:18:13 +0100

    Script from where the function should be ran

---
+++ script_test_functions.R
@@ -0,0 +1,11 @@
+
+# Divides the continuous data into the different forms as present in the redcap
+divide_forms(data = data)
+
+# Reshaping the data for eligibility
+
+reshape_eligibility(eligibility = eligibility)
+
+# sanity_check data eligibility 
+
+
