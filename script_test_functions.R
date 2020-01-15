1f557329419b7e9cb20276270098ce01e01968c4
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2020-01-15 12:21:41 +0100

    Include the function to source the other functions

--- script_test_functions.R
+++ script_test_functions.R
@@ -1,3 +1,15 @@
+#################################################################
+##            script data cleaning Swiss First data            ##
+#################################################################
+
+# Load the functions
+load_functions <- function(){
+  source("function_divide_forms.R")
+  source("reshaping_data/reshape_eligibility.R")
+  source("sanity_check_functions/SC_eligibility.R")
+}
+load_functions()
+  
 
 # Divides the continuous data into the different forms as present in the redcap
 divide_forms(data = data)
@@ -8,6 +20,7 @@ reshape_eligibility(eligibility = eligibility)
 
 # sanity_check data eligibility 
 
-
+rm(ed_first_consult, end_study_or_withdrawal, first_cs_after_ed, follow_up_2y, work_up)
+SC_eligibility(eligibility = eligibility)
 
 
