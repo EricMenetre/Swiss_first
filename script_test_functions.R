f633a1024abecc13f555164ba53b39016d07726b
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2020-01-16 15:04:34 +0100

    minor changes

--- script_test_functions.R
+++ script_test_functions.R
@@ -8,6 +8,7 @@ load_functions <- function(){
   source("reshaping_data/reshape_eligibility.R")
   source("sanity_check_functions/SC_eligibility.R")
   source("reshaping_data/reshape_ed_first_cs.R")
+  source("sanity_check_functions/SC_ed_first_cs.R")
 }
 load_functions()
   
@@ -24,5 +25,5 @@ reshape_ed_first_cs(ed_first_consult = ed_first_consult)
 # sanity_check data eligibility 
 
 SC_eligibility(eligibility = eligibility)
-
+SC_ed_first.cs(ed_first_consult = ed_first_consult)
 
