0ac562ef8f9864e74cc10a9361f21d074618f116
EricMenetre <33350889+ERPeric@users.noreply.github.com>
2020-02-20 17:11:04 +0100

    minor changes

--- script_test_functions.R
+++ script_test_functions.R
@@ -2,6 +2,8 @@
 ##            script data cleaning Swiss First data            ##
 #################################################################
 
+setwd("C:/Users/EricM/ownCloud/HUG/Projet sinergia Margitta/Data_analysis/Swiss_first")
+
 # Load the functions
 load_functions <- function(){
   source("function_divide_forms.R")
@@ -9,6 +11,8 @@ load_functions <- function(){
   source("sanity_check_functions/SC_eligibility.R")
   source("reshaping_data/reshape_ed_first_cs.R")
   source("sanity_check_functions/SC_ed_first_cs.R")
+  source("reshaping_data/reshape_workup.R")
+  source("sanity_check_functions/SC_workup.R")
 }
 load_functions()
   
@@ -20,10 +24,16 @@ divide_forms(data = data)
 
 reshape_eligibility(eligibility = eligibility)
 reshape_ed_first_cs(ed_first_consult = ed_first_consult)
+reshape_workup(work_up = work_up)
 
-
-# sanity_check data eligibility 
+# sanity_check
 
 SC_eligibility(eligibility = eligibility)
 SC_ed_first.cs(ed_first_consult = ed_first_consult)
+SC_workup(work_up = work_up)
 
+# Export sanity check information
+library(writexl)
+write_xlsx(SC_eligibility_output, "SC_outputs/SC_eligibility_output.xlsx")
+write_xlsx(SC_ed_first_output, "SC_outputs/SC_ed_first_output.xlsx")
+write_xlsx(SC_workup_output, "SC_outputs/SC_workup_output.xlsx")
